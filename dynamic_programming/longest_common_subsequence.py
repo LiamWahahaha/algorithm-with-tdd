659e16ab48f7580edd934b86560408a4ed82a43d
liam <lili.wang@rightwayhealthcare.com>
2022-05-22 15:49:03 -0400

    feat: Practice find lcs and find all lcs

--- dynamic_programming/longest_common_subsequence.py
+++ dynamic_programming/longest_common_subsequence.py
@@ -35,3 +35,66 @@ def calculate_lcs_length_bottom_up(text1, text2):
                 lookup[text1_idx][text2_idx] = max(lookup[text1_idx + 1][text2_idx], lookup[text1_idx][text2_idx + 1])
 
     return lookup[0][0]
+
+
+def find_lcs(text1, text2):
+    """
+    using bottom-up to fill the lookup table and then recursively finds lcs in
+    a top-down manner
+    """
+    lookup = [[0 for _ in range(len(text2) + 1)] for _ in range(len(text1) + 1)]
+
+    for text2_idx in range(1, len(text2) + 1):
+        for text1_idx in range(1, len(text1) + 1):
+            if text1[text1_idx - 1] == text2[text2_idx - 1]:
+                lookup[text1_idx][text2_idx] = lookup[text1_idx - 1][text2_idx - 1] + 1
+            else:
+                lookup[text1_idx][text2_idx] = max(lookup[text1_idx - 1][text2_idx], lookup[text1_idx][text2_idx - 1])
+
+    result = []
+
+    def lcs(text1_idx, text2_idx):
+        if text1_idx == 0 or text2_idx == 0:
+            return result.append("")
+        if text1[text1_idx - 1] == text2[text2_idx - 1]:
+            result.append(text1[text1_idx - 1])
+            return lcs(text1_idx - 1, text2_idx - 1)
+        elif lookup[text1_idx - 1][text2_idx] > lookup[text1_idx][text2_idx - 1]:
+            return lcs(text1_idx - 1, text2_idx)
+        else:
+            return lcs(text1_idx, text2_idx - 1)
+
+    lcs(len(text1), len(text2))
+    return ''.join(reversed(result))
+
+
+def find_all_lcs(text1, text2):
+    """
+    using bottom-up to fill the lookup table and then recursively finds lcs in
+    a top-down manner
+    """
+    lookup = [[0 for _ in range(len(text2) + 1)] for _ in range(len(text1) + 1)]
+
+    for text2_idx in range(1, len(text2) + 1):
+        for text1_idx in range(1, len(text1) + 1):
+            if text1[text1_idx - 1] == text2[text2_idx - 1]:
+                lookup[text1_idx][text2_idx] = lookup[text1_idx - 1][text2_idx - 1] + 1
+            else:
+                lookup[text1_idx][text2_idx] = max(lookup[text1_idx - 1][text2_idx], lookup[text1_idx][text2_idx - 1])
+
+    def lcs(text1_idx, text2_idx):
+        if text1_idx == 0 or text2_idx == 0:
+            return [""]
+        if text1[text1_idx - 1] == text2[text2_idx - 1]:
+            result = lcs(text1_idx - 1, text2_idx - 1)
+            for idx, ith_lcs in enumerate(result):
+                result[idx] = result[idx] + text1[text1_idx - 1]
+            return result
+        if lookup[text1_idx - 1][text2_idx] > lookup[text1_idx][text2_idx - 1]:
+            return lcs(text1_idx - 1, text2_idx)
+        elif lookup[text1_idx][text2_idx - 1] > lookup[text1_idx - 1][text2_idx]:
+            return lcs(text1_idx, text2_idx - 1)
+        else:
+            return lcs(text1_idx - 1, text2_idx) + lcs(text1_idx, text2_idx - 1)
+
+    return set(lcs(len(text1), len(text2)))
--- tests/test_dynamic_programming.py
+++ tests/test_dynamic_programming.py
@@ -1,12 +1,21 @@
 import unittest
 
-from dynamic_programming.longest_common_subsequence import calculate_lcs_length_top_down, calculate_lcs_length_bottom_up
+from dynamic_programming.longest_common_subsequence import calculate_lcs_length_top_down, \
+    calculate_lcs_length_bottom_up, find_lcs, find_all_lcs
 
 
 class TestLongestCommonSubsequence(unittest.TestCase):
     def test_calculate_lcs_length(self):
         self.assertEqual(3, calculate_lcs_length_top_down("ABCDEF", "BCESA"))
         self.assertEqual(3, calculate_lcs_length_bottom_up("ABCDEF", "BCESA"))
+
+    def test_find_lcs(self):
+        self.assertIn(find_lcs("XMJYAUZ", "MZJAWXU"), {"MJAU"})
+        self.assertIn(find_lcs("ABCBDAB", "BDCABA"), {"BCAB", "BCBA", "BDAB"})
+
+    def test_find_all_lcs(self):
+        self.assertEqual({"MJAU"}, find_all_lcs("XMJYAUZ", "MZJAWXU"))
+        self.assertEqual({"BCAB", "BCBA", "BDAB"}, find_all_lcs("ABCBDAB", "BDCABA"))
 
 
 if __name__ == '__main__':
